321528b2f406eaba8d0ff3cf557b0fe370c6c890
Ubuntu <ubuntu@ip-172-26-0-152.eu-central-1.compute.internal>
2019-01-21 19:31:17 +0000

    wip

--- saltedge/operation/SaltEdgeUser.php
+++ saltedge/operation/SaltEdgeUser.php
@@ -1,7 +1,24 @@
 <?php
 
-namespace SaltEdge;
+namespace SaltEdge\Operation;
 
-class SaltEdgeUser {
+use SaltEdge\Request\SaltEdge;
+
+class SaltEdgeUser extends Operation {
+    
+     /**
+     * @var string token operation endpoint for create
+     */
+    const ENDPOINT_CREATE = 'customers';
+    
+     /**
+     * @var array $response Response content after successful request
+     */
+    private $response;
+    
+    public function __construct(SaltEdge $connection)
+    {
+        parent::__construct($connection);
+    }
     
 }
